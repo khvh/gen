9bcf858efe0f5959a20dd765d5ee93ade79aad33
Jevgeni Sillar <js@khvh.dev>
2023-01-13 10:00:25 +0200

    add test gen

--- go.mod
+++ go.mod
@@ -6,14 +6,19 @@ require (
 	github.com/Masterminds/goutils v1.1.1 // indirect
 	github.com/Masterminds/semver v1.5.0 // indirect
 	github.com/Masterminds/sprig v2.22.0+incompatible // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/huandu/xstrings v1.4.0 // indirect
 	github.com/imdario/mergo v0.3.13 // indirect
-	github.com/mattn/go-colorable v0.1.12 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rs/zerolog v1.28.0 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
 	golang.org/x/crypto v0.5.0 // indirect
 	golang.org/x/sys v0.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -5,9 +5,10 @@ import (
 	"embed"
 	"flag"
 	"fmt"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"go/format"
 	"html/template"
-	"log"
 	"os"
 	"strings"
 
@@ -43,63 +44,99 @@ var (
 	in, out string
 )
 
-//go:embed main.tmpl
+//go:embed entity_gen.tmpl
 var mainTemplate embed.FS
 
+//go:embed entity_test_gen.tmpl
+var mainTestTemplate embed.FS
+
 func main() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
 	flag.StringVar(&in, "in", "spec.yml", "Spec file")
 	flag.StringVar(&out, "out", "internal/generated", "Output dir")
 
-	log.Default().Print(in, " <-> ", out)
+	log.Info().Msgf(in, " <-> ", out)
 
 	bts, err := os.ReadFile(in)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal().Err(err).Send()
 	}
 
 	ents := specEntities{}
 
-	yaml.Unmarshal(bts, &ents)
+	if err := yaml.Unmarshal(bts, &ents); err != nil {
+		log.Fatal().Err(err).Send()
+	}
 
 	err = os.RemoveAll("internal/generated/entities")
 	if err != nil {
-		log.Default().Print(err)
+		log.Fatal().Err(err).Send()
 	}
 
 	err = os.MkdirAll("internal/generated/entities", 0755)
 	if err != nil {
-		log.Default().Print(err)
+		log.Fatal().Err(err).Send()
 	}
-
-	// tmpl := template.Must(template.New("main").Funcs(sprig.FuncMap()).ParseFiles("main.tmpl"))
 
 	for _, item := range ents.Entities {
-		tmpl, err := template.New("").Funcs(sprig.FuncMap()).ParseFS(mainTemplate, "main.tmpl")
+		genEntity(&item)
+		genEntityTests(&item)
+	}
+}
 
-		if err != nil {
-			log.Fatal(err)
-		}
+func genEntity(item *specEntity) {
+	tmpl, err := template.New("").Funcs(sprig.FuncMap()).ParseFS(mainTemplate, "entity_gen.tmpl")
 
-		var processed bytes.Buffer
-
-		err = tmpl.ExecuteTemplate(&processed, "main.tmpl", &item)
-		if err != nil {
-			log.Fatalf("Unable to parse data into template: %v\n", err)
-		}
-
-		formatted, err := format.Source(processed.Bytes())
-		if err != nil {
-			log.Fatalf("Could not format processed template: %v\n", err)
-		}
-
-		path := fmt.Sprintf("internal/generated/entities/%s.go", strings.ToLower(item.Ref))
-
-		log.Default().Println(path)
-
-		os.WriteFile(path, formatted, 0755)
+	if err != nil {
+		log.Fatal().Err(err).Send()
 	}
 
-	// e := ent.New(ent.NewService(ent.WithRepository(generated.NewEntityMemoryRepository())))
+	var processed bytes.Buffer
 
-	// fmt.Println(e.TestMe())
+	err = tmpl.ExecuteTemplate(&processed, "entity_gen.tmpl", item)
+	if err != nil {
+		log.Fatal().Err(err).Send()
+	}
+
+	formatted, err := format.Source(processed.Bytes())
+	if err != nil {
+		log.Fatal().Err(err).Send()
+	}
+
+	path := fmt.Sprintf("internal/generated/entities/%s.go", strings.ToLower(item.Ref))
+
+	log.Info().Msgf("Writing %s -> %s", item.Ref, path)
+
+	if err := os.WriteFile(path, formatted, 0755); err != nil {
+		log.Fatal().Err(err).Send()
+	}
+}
+
+func genEntityTests(item *specEntity) {
+	tmpl, err := template.New("").Funcs(sprig.FuncMap()).ParseFS(mainTestTemplate, "entity_test_gen.tmpl")
+
+	if err != nil {
+		log.Fatal().Err(err).Send()
+	}
+
+	var processed bytes.Buffer
+
+	err = tmpl.ExecuteTemplate(&processed, "entity_test_gen.tmpl", item)
+	if err != nil {
+		log.Fatal().Err(err).Send()
+	}
+
+	formatted, err := format.Source(processed.Bytes())
+	if err != nil {
+		log.Fatal().Err(err).Send()
+	}
+
+	path := fmt.Sprintf("internal/generated/entities/%s_test.go", strings.ToLower(item.Ref))
+
+	log.Info().Msgf("Writing %s -> %s", item.Ref, path)
+
+	if err := os.WriteFile(path, formatted, 0755); err != nil {
+		log.Fatal().Err(err).Send()
+	}
 }
